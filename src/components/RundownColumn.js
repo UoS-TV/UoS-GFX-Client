ec7cb873dfa4afdbe701e1f226a311f90783b761
samsarj <samsarj@gmail.com>
2023-04-24 16:26:59 +0100

    added border for selected item

--- src/components/RundownColumn.js
+++ src/components/RundownColumn.js
@@ -5,18 +5,21 @@ import FloatingLabel from "react-bootstrap/FloatingLabel";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
 import GFXItem from "./GFXItem";
+import Card from "react-bootstrap/Card";
+import styled from "styled-components";
+
+export const BorderCard = styled(Card)`
+  background: ${(props) => (props.selected ? "#E8F1FF" : "#FFFFFF")};
+  border: ${(props) =>
+    props.selected ? "1.5px solid #2375A4" : "1px solid #d5dde3"};
+`;
 
 const RundownColumn = () => {
   const [rundown, setRundown] = useState("");
-  // const [apiKey, setapiKey] = useState("");
-  // const [apiToken, setapiToken] = useState("");
 
   const [options, setOptions] = useState([]);
 
-  const [sel, setSel] = useState("");
-
-  const [items, setItems] = useState([]);
-  const [selectedItem, setSelectedItem] = useState(null);
+  var results = [];
 
   // station = "surrey";
   // apiKey = "samsarjudeen";
@@ -33,7 +36,8 @@ const RundownColumn = () => {
     //     apiToken +
     // "&Action=getRundowns";
     const url =
-      "https://www.rundowncreator.com/surrey/API.php?APIKey=samsarjudeen&APIToken=VzPXfFFfTy6mvOVxmIy4EMpYlU84lI&Action=getRows&RundownID=" + rundown;
+      "https://www.rundowncreator.com/surrey/API.php?APIKey=samsarjudeen&APIToken=VzPXfFFfTy6mvOVxmIy4EMpYlU84lI&Action=getRows&RundownID=" +
+      rundown;
 
     console.log(url);
 
@@ -42,28 +46,37 @@ const RundownColumn = () => {
       .then((data) => setOptions(data));
   };
 
+  const [isSelected, setisSelected] = useState(0);
+
   return (
-        <Form onSubmit={handleFormSubmit}>
-          <InputGroup className="mb-3">
-            <FloatingLabel controlId="floatingInput" label="Rundown ID">
-              <Form.Control
-                type="number"
-                  // value={apiToken}
-                defaultValue={"143"}
-                onChange={(e) => setRundown(e.target.value)}
-                />
-            </FloatingLabel>
-            <Button variant="primary" type="submit">
-            Submit
-          </Button>
-          </InputGroup>
-            {options.map( option => {
-          return(
-            <GFXItem data={option}/>
-          )
-        })}
-        </Form>
-        
+    <Form onSubmit={handleFormSubmit}>
+      <InputGroup className="mb-3">
+        <FloatingLabel controlId="floatingInput" label="Rundown ID">
+          <Form.Control
+            type="number"
+            // defaultValue={"143"}
+            onChange={(e) => setRundown(e.target.value)}
+          />
+        </FloatingLabel>
+        <Button variant="primary" type="submit">
+          Get Rundown
+        </Button>
+      </InputGroup>
+      {options.map((option, index) => {
+        const results = [];
+        return (
+          <>
+            <GFXItem
+              data={option}
+              dyTags={results}
+              key={option.RowID}
+              selected={isSelected === option.RowID}
+              onChange={() => {setisSelected(option.RowID)}}
+            />
+          </>
+        );
+      })}
+    </Form>
   );
 };
 
