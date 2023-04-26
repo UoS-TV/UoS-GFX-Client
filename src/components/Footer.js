b040a31a50434a97a092bcf71102d9f663c13561
samsarj <samsarj@gmail.com>
2023-04-26 22:48:43 +0100

    added buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,18 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-// import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+
+import { UserContext } from "./context.jsx";
+import { useContext } from "react";
 
 const footerStyle = {
   backgroundColor: "#ececec",
   textAlign: "center",
-display: "flex",
-alignItems: "center",
+  display: "flex",
+  alignItems: "center",
   position: "fixed",
   left: "0",
   bottom: "0",
@@ -23,25 +25,50 @@ const phantomStyle = {
   width: "100%",
 };
 
-function Footer() {
+function Footer(props) {
+  const action = useContext(UserContext).actions;
+
   return (
     <div>
       <div style={phantomStyle} />
       <div style={footerStyle}>
         <Container>
           <Row>
-            {/* <Col sm={8}> */}
+            <Form
+              onSubmit={(event) => {
+                event.preventDefault();
+                action.play(event.target.command.value);
+              }}
+            >
               <InputGroup>
-                <Form.Control placeholder="CasparCG Command" />
-                <Button variant="warning">Send Command</Button>
+                <FloatingLabel
+                  controlId="floatingInput"
+                  label={"last command: " + props.value}
+                >
+                  <Form.Control name="command" />
+                </FloatingLabel>
+                <Button variant="warning" type="submit">
+                  Send Command
+                </Button>
+                {[...Array(props.channels)].map((e, value) => {
+                  return (
+                    <Button
+                      key={value}
+                      variant="secondary"
+                      onClick={() => action.custom("CLEAR " + (value + 1))}
+                    >
+                      CLEAR CH. {value + 1}
+                    </Button>
+                  );
+                })}
+                <Button
+                  variant="dark"
+                  onClick={() => action.custom("CLEAR ALL")}
+                >
+                  CLEAR ALL
+                </Button>
               </InputGroup>
-            {/* </Col> */}
-            {/* <Col sm={4}>
-              <ButtonGroup className="">
-                <Button variant="primary">Save Rundown</Button>
-                <Button variant="info">Load Rundown</Button>
-              </ButtonGroup>
-            </Col> */}
+            </Form>
           </Row>
         </Container>
       </div>
