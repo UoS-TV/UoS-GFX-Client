1c0c89435a6a3e179c673dc6c7f20184024a1b95
samsarj <samsarj@gmail.com>
2023-04-30 22:16:56 +0100

    added caspar functions and item retrievals

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 
 import React, { useState } from "react";
 
+import axios from "axios";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
@@ -13,27 +15,114 @@ import Footer from "./components/Footer";
 import { UserContext } from "./components/context";
 
 import configData from "./config.json";
+import FileSaver from "file-saver";
 
 const channels = configData.channels;
+
+const handleSaveToPC = (jsonData, filename) => {
+  var blob = new Blob([JSON.stringify(jsonData, null, 2)], {
+    type: "application/json",
+  });
+  FileSaver.saveAs(blob, filename);
+};
+
+const dyTagHandler = (dyTags) => {
+  var obj = {};
+  dyTags.forEach((dyTag) => {
+    obj[dyTag.dyID] = dyTag.dyData;
+  });
+  console.log(obj);
+  // escaping function
+  return JSON.stringify(obj).replace(/"/g, '\\"');
+};
 
 const App = () => {
   const [commandBox, setCommandBox] = useState("");
 
+  const handleSendToServer = (data) => {
+    console.log("handle send to server function called!");
+    axios
+      .post("http://localhost:3001/data", { data: data })
+      .then((response) => {
+        console.log(response.data);
+        // setCommandBox(response.data); //THIS DOES WEIRD STUFF AND TRIGGERS ANOTHER FUNTION
+      });
+  };
+
+  const getItems = (data) => {
+    console.log("get items server function called!");
+    axios
+      .post("http://localhost:3001/filegetter", { type: data })
+      .then((response) => {
+        var _items = items;
+        _items[data] = response.data;
+        setItems(_items);
+        console.log("Response: ", response.data);
+        console.log(items);
+      })
+      .catch((error) => {
+        var _items = items;
+        console.log("got errr while posting data", error);
+        _items[data] = [`Start server link and refresh ${data}`];
+        setItems(_items);
+        console.log(items);
+      });
+  };
+
+  const [items, setItems] = useState({
+    templates: getItems("templates"),
+    media: [getItems("media")],
+  });
+
   const allContexts = {
-    actions: {
-      custom: (command) => {
-        console.log("let's gooo", command);
-        setCommandBox(command);
-      },
+    itemActions: {
       play: (command) => {
-        console.log("let's gooo", command);
-        setCommandBox(command);
+        handleSendToServer(
+          "CG " +
+            command.channel +
+            "-" +
+            command.layer +
+            " ADD 1 " +
+            command.template +
+            " 1 " +
+            '"' +
+            dyTagHandler(command.dyTags) +
+            '"'
+        );
+      },
+      next: (command) => {
+        console.log("NEXT FUNCTION");
+      },
+      update: (command) => {
+        handleSendToServer(
+          "CG " +
+            command.channel +
+            "-" +
+            command.layer +
+            " UPDATE 1 " +
+            '"' +
+            dyTagHandler(command.dyTags) +
+            '"'
+        );
+        console.log("Test update", command);
+      },
+      stop: (command) => {
+        handleSendToServer(
+          "CG " + command.channel + "-" + command.layer + " STOP 1 "
+        );
+        console.log("Test stop", command);
+      },
+      custom: (command) => {
+        handleSendToServer(command);
       },
     },
     api: configData.api,
     defaultChannel: 1,
     defaultLayer: 20,
     channelMax: channels,
+    handleSave: handleSaveToPC,
+    items: items,
+    getItems: getItems,
   };
 
   return (
@@ -44,12 +133,12 @@ const App = () => {
           <Route exact path="/" element={<MainSection />} />
           {/* <Route path="/RundownSelector" element={<RundownSelector />} /> */}
         </Routes>
-        <Footer value={commandBox} channels={channels} />
+        <Footer channels={channels} />
       </Router>
     </UserContext.Provider>
   );
 };
 
 const container = document.getElementById("root");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 root.render(<App />);
