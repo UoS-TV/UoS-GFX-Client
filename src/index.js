4ba3a6840ceec8367ca490d53e463d6e3d7c234f
samsarj <samsarj@gmail.com>
2023-05-04 17:13:14 +0100

    header built in

--- src/index.js
+++ src/index.js
@@ -2,22 +2,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import { createRoot } from "react-dom/client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import axios from "axios";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Header from "./components/Header";
-import MainSection from "./components/MainSection";
+import Header from "./components/old/Header";
+import MainSection from "./components/old/MainSection";
 import Footer from "./components/Footer";
 
 import { UserContext } from "./components/context";
 
-import configData from "./config.json";
+import config from "./config.json";
 import FileSaver from "file-saver";
+import { Button, Container, Navbar } from "react-bootstrap";
+import CustomColumn from "./components/CustomColumn";
 
-const channels = configData.channels;
+const channels = config.channels;
 
 const handleSaveToPC = (jsonData, filename) => {
   var blob = new Blob([JSON.stringify(jsonData, null, 2)], {
@@ -37,42 +39,53 @@ const dyTagHandler = (dyTags) => {
 };
 
 const App = () => {
-  const [commandBox, setCommandBox] = useState("");
+  const [commandBox, setCommandBox] = useState({ last: "", response: "" });
+
+  const [items, setItems] = useState({
+    templates: [],
+    media: [],
+  });
 
   const handleSendToServer = (data) => {
-    console.log("handle send to server function called!");
+    console.log(data);
     axios
       .post("http://localhost:3001/data", { data: data })
       .then((response) => {
         console.log(response.data);
-        // setCommandBox(response.data); //THIS DOES WEIRD STUFF AND TRIGGERS ANOTHER FUNTION
+        let _commandBox = commandBox;
+        _commandBox.last = data;
+        _commandBox.response = response.data;
+        setCommandBox(_commandBox); //THIS DOES WEIRD STUFF AND TRIGGERS ANOTHER FUNTION
+        console.log(commandBox);
       });
   };
 
-  const getItems = (data) => {
-    console.log("get items server function called!");
+  const getItems = () => {
     axios
-      .post("http://localhost:3001/filegetter", { type: data })
+      .post("http://localhost:3001/filegetter")
       .then((response) => {
         var _items = items;
-        _items[data] = response.data;
+        _items = response.data;
         setItems(_items);
         console.log("Response: ", response.data);
-        console.log(items);
       })
       .catch((error) => {
         var _items = items;
         console.log("got errr while posting data", error);
-        _items[data] = [`Start server link and refresh ${data}`];
+        _items = [`Start server link and refresh`];
         setItems(_items);
-        console.log(items);
       });
   };
 
-  const [items, setItems] = useState({
-    templates: getItems("templates"),
-    media: [getItems("media")],
-  });
+  // get library file on page load, don't get on re-render of component
+  useEffect(() => {
+    let ignore = false;
+
+    if (!ignore) getItems();
+    return () => {
+      ignore = true;
+    };
+  }, []);
 
   const allContexts = {
     itemActions: {
@@ -104,37 +117,47 @@ const App = () => {
             dyTagHandler(command.dyTags) +
             '"'
         );
-        console.log("Test update", command);
       },
       stop: (command) => {
         handleSendToServer(
           "CG " + command.channel + "-" + command.layer + " STOP 1 "
         );
-        console.log("Test stop", command);
       },
       custom: (command) => {
         handleSendToServer(command);
       },
     },
-    api: configData.api,
+    api: config.api,
     defaultChannel: 1,
     defaultLayer: 20,
     channelMax: channels,
     handleSave: handleSaveToPC,
     items: items,
-    getItems: getItems,
   };
 
   return (
     <UserContext.Provider value={allContexts}>
-      <Router>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<MainSection />} />
-          {/* <Route path="/RundownSelector" element={<RundownSelector />} /> */}
-        </Routes>
-        <Footer channels={channels} />
-      </Router>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">UoS Caspar Client</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            {/* <Nav className="me-auto"> */}
+            {/* <Nav.Link href="/">Home</Nav.Link> */}
+            {/* <Nav.Link href="/RundownSelector">Rundown Selector</Nav.Link> */}
+            {/* </Nav> */}
+          </Navbar.Collapse>
+          <Button variant="outline-info" size="sm" onClick={getItems}>
+            Refresh Library
+          </Button>
+          {/* <BsFillGearFill variant="primary" onClick={handleShow} />
+        <ConfigModal isModalVisible={show} handleShow={() => setShow(false)} /> */}
+        </Container>
+      </Navbar>
+      <Container>
+        <CustomColumn />
+      </Container>
+      <Footer channels={channels} command={commandBox} />
     </UserContext.Provider>
   );
 };
